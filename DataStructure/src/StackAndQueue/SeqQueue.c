d22975d02a477e9647069a8cca6d0e83f4353f92
Yicheng Ye <ye.yicheng123@gmail.com>
2014-11-04 21:54:13 +0800

    done with seq queue

---
+++ DataStructure/src/StackAndQueue/SeqQueue.c
@@ -0,0 +1,43 @@
+#define QueueSize 100
+typedef struct {
+	char data[QueueSize];
+	int front, rear;
+} SeqQueue;
+
+void initSeqQueue(SeqQueue *queue) {
+	queue->front = queue->rear = 0;
+}
+
+int seqQueueIsEmpty(SeqQueue *queue) {
+	return queue->front == queue->rear;
+}
+
+int seqQueueIsFull(SeqQueue *queue) {
+	return (queue->rear + 1) % QueueSize == queue->front;
+}
+
+void seqEnqueue(SeqQueue *queue, char x) {
+	if (seqQueueIsFull(queue)) {
+		printf("queue is full");
+		return;
+	}
+	queue->data[queue->rear++] = x;
+}
+
+char seqQueueDequeue(SeqQueue *queue) {
+	if (seqQueueIsEmpty(queue)) {
+		printf("queque is empty");
+		exit(0);
+	}
+	char temp = queue->data[queue->front];
+	queue->front = (queue->front + 1) % QueueSize;
+	return temp;
+}
+
+char peek(SeqQueue *queue) {
+	if (seqQueueIsEmpty(queue)) {
+		printf("queque is empty");
+		exit(0);
+	}
+	return queue->data[queue->front];
+}
